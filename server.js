c769bf7bd597242e0166cd60727088f325d8e359
Your Name <angelicrf@gmail.com>
2020-02-06 14:04:19 -0800

    WMongoDBSetup

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const users = require('./routes/Users');
 const auth = require('./routes/Auth');
 const contacts = require('./routes/Contacts');
-
+const connectDB = require('./config/db');
 const app = express();
 const port = process.env.PORT;
+
 
 app.get('/', (req, res) => res.json({msg: 'Succeed...'}));
 
@@ -13,3 +14,4 @@ app.use('/api/auth', auth);
 app.use('/api/contacts', contacts);
 
 app.listen(port, () => console.log(`The server is listenning on port: ${port}...`));
+connectDB;
