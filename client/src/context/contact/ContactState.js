bb743f9cb84c05e61b680b6f13b333b07b6f3b98
Your Name <angelicrf@gmail.com>
2020-02-20 10:21:33 -0800

    WAdd-Contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,10 +37,15 @@ const ContactState = props => {
     };
 
     const [state, dispatch] = useReducer(ContactReducer, initialState);
-
+    const addContact = contact => {
+        contact.id = uuid.v4();
+        dispatch({ type: ADD_CONTACT, payload: contact })
+    };
         return (
        <ContactContext.Provider value={{
-           contacts: state.contacts
+           contacts: state.contacts,
+           addContact
+
        }}>
            {props.children}
        </ContactContext.Provider>
